434c250d35636bbe1cdc9fb07e7d20beb2ed7fc6
Mohd Aakib <121683571+Aaquibb@users.noreply.github.com>
2025-09-28 05:21:27 +0000

    datatypes

---
+++ 01_basics/01_code_variables.js
@@ -0,0 +1,6 @@
+var x='Hi there!';
+const account_id=12345;
+let a;
+// account_id=x;
+console.log(account_id,x);
+console.table([x,account_id,'hiiiiiiiii',a]);
---
+++ 01_basics/01_datatypes.js
@@ -0,0 +1,4 @@
+let a=0;let b=10;
+console.log(typeof "a");
+console.log(typeof null);
+console.table([null, undefined]);
