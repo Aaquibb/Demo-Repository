3200c5c4e15196ffdd4c011d7fe82ec03f2e849d
Mohd Aakib <121683571+Aaquibb@users.noreply.github.com>
2025-09-28 05:53:02 +0000

    conversion of datatypes

--- 01_basics/01_datatypes.js
+++ 01_basics/01_datatypes.js
@@ -1,4 +1,18 @@
-let a=0;let b=10;
-console.log(typeof "a");
-console.log(typeof null);
-console.table([null, undefined]);
+// let a=0;let b=10;
+// console.log(typeof "a");
+// console.log(typeof null);
+// console.table([null, undefined]);
+// let 
+// score="293846239746";
+// console.log(Number(score));
+// console.log(typeof score);
+// console.log(score);
+
+let isA=-0.2374;
+let isB="";
+console.log(isA);
+console.log(typeof isA);
+console.log(Boolean(isA));
+console.log(typeof isA);
+console.log(Boolean(isB));
+console.log(typeof isB);
